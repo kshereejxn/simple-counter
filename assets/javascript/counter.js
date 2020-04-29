9407b5b24d8bc0b2f2eed9f76c591d0a52ba4034
Trinie J <jxsn00ks@gmail.com>
2020-04-28 20:24:25 -0400

    added value to the html counter

--- assets/javascript/counter.js
+++ assets/javascript/counter.js
@@ -8,4 +8,17 @@ let counter = document.querySelector("#counter");
 // Use the console.log function to display some text in the console when the button is clicked:
 incrementButton.addEventListener("click", function() {
     console.log("+ button clicked");
-  });
+
+  // Calculate the new value for our counter:
+  
+  let newCounterValue = parseInt(counter.innerHTML) + 1;
+  // Calculate the new value for our counter:
+  counter.innerHTML = newCounterValue;
+
+  // If the counter is >= 10 then change the text color to red:
+  if(newCounterValue >= 10) {
+    counter.style.color = "red";
+  }
+  // Calculate the new value for our counter:
+  counter.innerHTML = newCounterValue;
+})
